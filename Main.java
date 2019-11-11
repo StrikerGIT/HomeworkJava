793b64c9f16f11c1e2ff6472677d6acf8ca2c085
StrikerGIT <Lyaschenkoilja@gmail.com>
2019-11-11 22:21:05 +0300

    Загрузил дз 8

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+    Forester forester = new Forester();
+    int [] arrayInt = {1,2,3,2,5,3,2,1,2,3,3,3,2,1,1,4,3,2,1};
+    forester.start(arrayInt);
+    }
+}
