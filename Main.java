2a462ac953fb0b8050d472936439818298edbcfb
StrikerGIT <Lyaschenkoilja@gmail.com>
2019-11-18 01:19:19 +0300

    Чищу старое

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    Forester forester = new Forester();
-    int [] arrayInt = {1,2,3,2,5,3,2,1,2,3,3,3,2,1,1,4,3,2,1};
-    forester.start(arrayInt);
-    }
-}
