7d3cfa099b30cafdbf6b51da32efd8861c3525d6
StrikerGIT <Lyaschenkoilja@gmail.com>
2020-06-23 15:10:12 +0300

    HW 4

---
+++ EE-lesson-4/src/main/java/geekbrains/persist/Product.java
@@ -0,0 +1,43 @@
+package geekbrains.persist;
+
+public class Product {
+
+    private Long id;
+
+    private String title;
+
+    private Integer coast;
+
+    public Product() {
+    }
+
+    public Product(Long id, String title, Integer coast) {
+        this.id = id;
+        this.title = title;
+        this.coast = coast;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getCoast() {
+        return coast;
+    }
+
+    public void setCoast(Integer targetDate) {
+        this.coast = coast;
+    }
+}
