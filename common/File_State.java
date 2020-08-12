0ba0f77a33038ab49736f859e0cee63c7c0ac18b
StrikerGIT <Lyaschenkoilja@gmail.com>
2020-08-12 15:29:07 +0300

    Clean

--- common/File_State.java
+++
@@ -1,5 +0,0 @@
-package com.striker.geekcloud.common;
-
-public enum File_State {
-    NAME_LENGTH, NAME, FILE_LENGTH, FILE
-}
--- common/State.java
+++
@@ -1,5 +0,0 @@
-package com.striker.geekcloud.common;
-
-public enum State {
-    WAIT_COMMAND, WAIT_FILE, SENT_FILE
-}
