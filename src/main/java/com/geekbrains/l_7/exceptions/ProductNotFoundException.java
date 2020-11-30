9c23831e489a66c07a71808106e9ac431635660b
StrikerGIT <Lyaschenkoilja@gmail.com>
2020-11-30 16:38:38 +0300

    Clean

--- src/main/java/com/geekbrains/l_7/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.l_7.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
